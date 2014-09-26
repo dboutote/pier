614da5a68b994708e8cbe7c75e4d6bb85b8b7e99
Kevincredible <kevin.beronilla@gmail.com>
2014-09-26 17:14:30 -0500

    Update page-landing-page.php
    Changed the header/intro section to be a full column and removed the right column (containing the Share & Print buttons and Featured Promotion). The Featured Promotion option needs to be removed when editing a Page.

--- application/wp-content/themes/navypier/page-landing-page.php
+++ application/wp-content/themes/navypier/page-landing-page.php
@@ -26,7 +26,7 @@ if( has_post_thumbnail() ){
 
 	<div class="intro clearfix">
 		
-		<div class="col span-2 padded">
+		<div class="col span-3 padded">
 			<?php
 				// Start the Loop.
 				while ( have_posts() ) : the_post();
@@ -36,6 +36,8 @@ if( has_post_thumbnail() ){
 			?>
 		</div>
 		
+		
+	<?php /*
 		<div class="col padded">
 			<div class="page-actions align-right">
 				<a href="#" data-shareid="<?php echo $post->ID;?>" class="icon share">share page</a><a href="#" class="icon print">print page</a>
@@ -45,6 +47,7 @@ if( has_post_thumbnail() ){
 				<?php mbp_display_promotion_box(); ?>
 			<?php }?>
 		</div>
+	*/ ?>
 		
 	</div> <!-- /.intro -->
 
