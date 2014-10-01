64283f1ee69ea376fde1663561f35c5dcf8f0510
Kevincredible <kevin.beronilla@gmail.com>
2014-10-01 12:25:55 -0500

    Update header.php
    Disabled BlackBerry and iOS auto phone number detection

--- application/wp-content/themes/navypier/header.php
+++ application/wp-content/themes/navypier/header.php
@@ -21,6 +21,8 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="format-detection" content="telephone=no">
+<meta http-equiv="x-rim-auto-match" content="none">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
