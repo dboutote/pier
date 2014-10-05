e59f481eedea5d7910b5b48b70ff39fd1311091d
dboutote <dboutote@gmail.com>
2014-10-05 11:00:37 -0500

    updated mailchimp

--- application/wp-content/themes/navypier/form-newsletter.php
+++ application/wp-content/themes/navypier/form-newsletter.php
@@ -1,9 +1,9 @@
-<?php $action_url = 'http://hirecause.us3.list-manage.com/subscribe/post-json?u=03fdcd45c897b8a631ebab0db&amp;id=025d759e3e'; ?>
+<?php $action_url = '//navypier.us1.list-manage.com/subscribe/post-json?u=ae16c850129383c3474359a1d&amp;id=06072d156b'; ?>
 
 <form action="<?php echo $action_url; ?>" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-horizontal subscribeform clearfix" novalidate>
 	<input type="text" name="EMAIL" placeholder="Enter Email Address" required>
 	<input type="submit" name="Submit" value="Subscribe">
 	<div class="response"></div>
 	<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-	<div style="position: absolute; left: -5000px;"><input type="text" name="b_03fdcd45c897b8a631ebab0db_025d759e3e" value=""></div>				
+	<div style="position: absolute; left: -5000px;"><input type="text" name="b_ae16c850129383c3474359a1d_06072d156b" value=""></div>				
 </form>
